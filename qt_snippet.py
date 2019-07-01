09c74085cd01a749c849f4debd5d58001141b3d3
Tung <tungtanium@users.noreply.github.com>
2019-07-01 15:29:24 +1200

    Update qt_snippet.py

--- qt_snippet.py
+++ qt_snippet.py
@@ -1,2 +1,21 @@
 import sys
 from Qt import QtWidgets, QtCore
+
+
+class BaseWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(BaseWindow, self).__init__()
+        self.initUI()
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Base Window')
+        self.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    w = BaseWindow()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
